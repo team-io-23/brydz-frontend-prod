2fd4396b108e86f2c594c83bbba89e288449bd97
mkostyk <m.kostyk22@gmail.com>
2023-07-15 16:35:28 +0200

    Updated config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-//export const SERVER = "https://brydz-backend-f4d416396c7b.herokuapp.com/"
-export const SERVER = "http://localhost:8000/"
+export const SERVER = "https://brydz-backend-f4d416396c7b.herokuapp.com/"
+//export const SERVER = "http://localhost:8000/"
